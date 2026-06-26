1b3bf951ae1b02292dcecf814b10c8f84a753ea4
chonline <chonline2023@163.com>
2026-06-26 16:37:17 +0800

    Add main Application class

---
+++ src/main/java/com/quxuanji/QuXuanJiApplication.java
@@ -0,0 +1,11 @@
+package com.quxuanji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuXuanJiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QuXuanJiApplication.class, args);
+    }
+}
